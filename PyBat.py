bb7e18075c02454c86358ea5f4ee9149a04af77b
hennika <37066271+hennika@users.noreply.github.com>
2019-01-17 22:31:34 +0100

    - Rewritten/added stuff in PyBat. - Added possibility of exporting data variables as Excel/CSV files in PyBat and support.

--- PyBat.py
+++ PyBat.py
@@ -23,27 +23,29 @@ def About(data_storage, CellKey):
     return
 
 #Locations of folders: Can be imported by writing "from PyBat import Database"
-Database = 'C:/users/andnor/OneDrive - NTNU/Diatoma/Experimental/Database/'                         #Location where data frames are stored.
-CellDatabase = Path('C:/Users/andnor/OneDrive - NTNU/Diatoma/Experimental/Database/CellDatabase')            # Location where cell data frames are stored.
+# Andreas
+#Database = 'C:/users/andnor/OneDrive - NTNU/Diatoma/Experimental/Database/'                         #Location where data frames are stored.
+#CellDatabase = Path('C:/Users/andnor/OneDrive - NTNU/Diatoma/Experimental/Database/CellDatabase')            # Location where cell data frames are stored.
+#Plots = 'C:/Users/andnor/OneDrive - NTNU/Diatoma/Experimental/Plots/'                                 # Location wher plots are stored
+#raw_data = Path(r"C:\Users\andnor\OneDrive - NTNU\Diatoma\Experimental\Experimental data\Galvanostatic Cycling")   # Location where raw data from experiments are located
 
-Plots = 'C:/Users/andnor/OneDrive - NTNU/Diatoma/Experimental/Plots/'                                 # Location wher plots are stored
-raw_data = Path(r"C:\Users\andnor\OneDrive - NTNU\Diatoma\Experimental\Experimental data\Galvanostatic Cycling")   # Location where raw data from experiments are located
-
-
+# Henning
+Database = 'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/'                     # Location where data frames are stored.
+CellDatabase = Path('C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Pickles')    # Location (as path) where cell data frames are stored.
+CellDatabase_string = 'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Pickles'   # Location (as string) where cell data frames are stored.Plots = 'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Graphs'                  # Location where plots are stored
+raw_data = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Raw_files')     # Location where raw data from experiments are located
+exported_data = 'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Exported'
 
 #------------------------------General info-------------------------
 
+#Input/Conversion of data
 
-#Innput/Conversion of data
-
-
-
-search_word = "SiO2MSC2_16_LongP1"
-
+#search_word = 'CB'
+#support.search_file(search_word, raw_data)    # search_word: Key word which will be search for. CellDatabase/raw_data: Location that will be searched in.
 
 #support.search_file(search_word, raw_data)    # search_word: Key word which will be search for. CellDatabase/raw_data: Location that will be searched in.
 
-support.automatic_conversion(search_word,raw_data,CellDatabase)
+#support.automatic_conversion(search_word,raw_data,CellDatabase)
 #support.merge_biologic(search_word, CellDatabase)
 
 #------------------------------Plotting------------------------------
@@ -52,18 +54,15 @@ support.automatic_conversion(search_word,raw_data,CellDatabase)
 #
 #
 #
-# # #
+pickle_name_1 = '/Users\hennika\OneDrive - NTNU\PhD\Results\Cycling\Pickles\B1_combi_t02_01_APC-THF_2_4Vto0_2V_0_01C'
+pickle_name_2 = '/Users\hennika\OneDrive - NTNU\PhD\Results\Cycling\Pickles\B1_combi_t03_01_LiBH4_1_7to0_2V_0_01C_CE1'
 
-
-# #
-
-
-
-
-
-
-#Plotter.plotter(pickle1=pickle_name_1, pickle2=pickle_name_2, x1='cap_incr_spec', y1='potential', cycles1=[0,1,5,10], color1='blue', color_scheme2='magma',legend=['Cell 1', 'Cell 2'], legend_loc=1)
-
+# Simple plotting:
+#Plotter.plotter(pickle1=pickle_name_1, pickle2=pickle_name_2, x1='cap_incr_spec', y1='potential', legend=['Cell 1', 'Cell 2'])
+# Advanced:
+custom = 'plt.text(50,1,\'So science\') \nplt.text(90,1.75,\'Much wow\')'       # Code that will be executed to customize plot (add text, arrows etc).
+plot1_path = '/Users\hennika\OneDrive - NTNU\PhD\Results\Cycling\Graphs\Plot1'  # Name of plot including full path
+# Plotter.plotter(pickle1=pickle_name_1, pickle2=pickle_name_2, xlim=[0,150], ylim=[0.1,2.2], xticks=[0,50,100,150], yticks=[0.2, 1, 1.5, 2.1], xlabel='Capacity (mAh/g active material)',ylabel='Potential vs Mg/Mg$^{2+}$', x1='cap_incr_spec', y1='potential', cycles1=[0,1,5,10], color1='blue', color_scheme2='magma',legend=['Cell 1', 'Cell 2'], legend_loc=1, custom_code=custom, save_path=plot1_path)
 
 
 
@@ -75,5 +74,14 @@ support.automatic_conversion(search_word,raw_data,CellDatabase)
 
 #print(output)
 
+#---------------------Exporting Data-------------------
 
+# Export data for e.g. plotting using other software:
+# Uses CellDatabase_string to find pickle and exported_data as destination
+pickle = 'B1_combi_t02_01_APC-THF_2_4Vto0_2V_0_01C'     # Pickle to be exported.
+export_variables = ['cap_incr_spec', 'potential']       # As many as you like
 
+support.export_data(CellDatabase_string, pickle, exported_data, export_variables)
+
+#df = pd.read_pickle(pickle_name_1)  # Reads pickle
+#print(df['cap_incr_spec'])
--- support.py
+++ support.py
@@ -166,7 +166,34 @@ def error_message(string):
 
     return
 
+# Export data for e.g. plotting using other software.
+def export_data(pickle_path, pickle_name, destination, *argv):
+    df = pd.read_pickle(pickle_path+'\\'+pickle_name)  # Reads pickle
+    if not os.path.exists(destination):     # Making destination folder if it does not exists already.
+        os.makedirs(destination)
+    #writer = pd.ExcelWriter(destination+'\\'+pickle_name+'.xlsx', engine='xlsxwriter')
+    #counter = 0         # Counter needed for ExcelWriter to add next variable in next column (and not overwrite it).
+    for arg in argv:     # Iterate through all variables specified
+        try:
+            # If Excel:
+            #df[arg].to_excel(writer, startcol=counter, index=False, freeze_panes=[1,0])     # Freezes top row, for easy reading.
+            #counter = counter + 1      # Counter needed to add next variable in next column (and not overwrite it).
 
+            # If csv-file:
+            df[arg].to_csv(destination + '\\' + pickle_name + '.csv', sep='\t', float_format='%f', encoding='utf-8', index=False)
 
+        except:
+            error_message('Error in reading/writing the variable to export')
 
+    # Add some cell formats for prettier values in Excel:
+    # OBS! Comma and scientific writing may cause some problems.
+    #workbook = writer.book
+    #worksheet = writer.sheets['Sheet1']
+    #format1 = workbook.add_format({'num_format': '0.00'})
+    #worksheet.set_column('A:AA', None, format1) # None argument refers to column width.
+    #writer.save()
+
+    print (pickle_name + '.xlsx \n \t exported to \n' + destination)
+
+    return
 
