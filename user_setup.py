9e62d10bdfd376de7107b2a281cf6356eb71ef1e
AndreasNorberg <andreasnnorberg@gmail.com>
2019-03-02 15:59:43 +0100

    updating function and file names according to convention

--- user_setup.py
+++ user_setup.py
@@ -6,16 +6,16 @@ from pathlib import Path
 # Fill in the locations of your folders using the variables below (Henning's files as examples)
 # ----------------------------------------------------------------------------------------------
 # Raw data (text files):
-raw_data = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Raw_files')
+raw_data = Path(r'C:\Users\andnor\OneDrive - NTNU\Diatoma\Experimental\Experimental data\Galvanostatic Cycling')
 
 # Database where the imported cells will be (when saved as pickle)
-database = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Pickles')
+database = Path(r'C:/Users/andnor/OneDrive - NTNU/Diatoma/Experimental/Database/CellDatabase/')
 
 # (Default) Location to where the exported data will be (possible to override)
-exported_data = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Exported')
+#exported_data = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Exported')
 
 # (Default) Location to where plots will be saved (possible to override)
-plots = Path(r'C:/Users/hennika/OneDrive - NTNU/PhD/Results/Cycling/Plots')
+plots = Path(r'C:\Users\andnor\OneDrive - NTNU\Diatoma\Experimental\Results\Plots')
 
 # Histogram size for calculating differential capacity
 # Should be significantly larger than the largest step in potential values.
