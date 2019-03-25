d9be1c46c853654586970aaf0fe59722ba4d246d
hennika <37066271+hennika@users.noreply.github.com>
2019-03-25 13:17:36 +0100

    Default is running.

--- testing.py
+++ testing.py
@@ -2,9 +2,9 @@
 import automate
 
 
-search_word = 'test'                                     # Maccor file
-#automate.auto_import(search_word, testing=True)
-"""automate.auto_plot(
+search_word = 'test'
+automate.auto_import(search_word, testing=True)
+automate.auto_plot(
                    search_word, 
                    testing = True,
                    x1 = 'cap_incr_spec',
@@ -16,4 +16,3 @@ search_word = 'test'                                     # Maccor file
                    #cycles3 = [0, 1, 10],
                    #legend = ['Cell 1', 'Cell 2', 'Cell 3']
                    )
-"""
